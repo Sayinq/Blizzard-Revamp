39526d7be363f4c0033fb9213804a43c9757545b
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-11-18 09:07:11 -0500

    Initial file commits

---
+++ blizzard-landing/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <main>
+            <section>
+                <h2 className="text-4xl text-red font-bold">Test Text</h2>
+            </section>
+        </main>
+    )
+}
+
+export default Home
