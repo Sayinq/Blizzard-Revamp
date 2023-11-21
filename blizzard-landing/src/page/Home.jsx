323c78f44c6bf1c645ae831d834924557e596bc3
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-11-21 12:01:35 -0500

    Hero section with some text and overlay

--- blizzard-landing/src/page/Home.jsx
+++ blizzard-landing/src/page/Home.jsx
@@ -3,6 +3,8 @@ import NavbarComp from '../components/NavbarComp'
 import FeaturedGames from '../components/FeaturedGames';
 import About from '../components/About';
 import News from '../components/News';
+import heroBG from '../assets/illidan-hero.png';
+import '../index.css';
 
 const Home = () => {
     return (
@@ -11,8 +13,17 @@ const Home = () => {
 
             {/* Hero Section */}
             {/* Maybe make this like Buddha threeJS parallax with one of my assets */}
-            <section className="w-screen h-screen">
-                <h2 className="text-4xl font-bold">Test Text</h2>
+            <section className="w-screen h-screen flex items-center justify-center">
+                <div id="gradient-overlay" className="absolute top-0 left-0 w-full h-full z-20"></div>
+                <img
+                    src={heroBG}
+                    className="absolute top-0 left-0 w-full h-full object-cover z-10"
+                    alt="Hero Background"
+                />
+                <div id="hero-text" className="flex flex-col justify-center items-center gap-y-8 z-30">
+                    <span className="xl:text-7xl md:text-5xl text-3xl font-bold">CTRL+ALT+DEFEAT</span>
+                    <span className="xl:text-3xl md:text-xl text-base font-semibold">Your Adventure Starts Now</span>
+                </div>
             </section>
 
             {/* Featured Section */}
