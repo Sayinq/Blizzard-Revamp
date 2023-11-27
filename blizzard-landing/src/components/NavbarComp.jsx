1e49cf223b17a138241db860ac706ffa4fe85227
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-11-27 14:03:01 -0500

    Better navbar with breakpoints and dropdowns

--- blizzard-landing/src/components/NavbarComp.jsx
+++ blizzard-landing/src/components/NavbarComp.jsx
@@ -4,7 +4,9 @@ import { Button,
         Dropdown,
         DropdownTrigger,
         DropdownMenu,
-        DropdownItem } from '@nextui-org/react';
+        DropdownItem,
+        DropdownSection,
+        User } from '@nextui-org/react';
 
 
 
@@ -18,8 +20,8 @@ import chevronLogo from '../assets/chevron-down-outline.svg';
 const NavbarComp = () => {
     return (
         <nav id="navbar" className="fixed top-0 left-0 flex items-center w-screen h-[100px] bg-[#1A1A1A] z-50">
-            <div id="nav-wrapper" className="absolute flex flex-row justify-between items-center w-full h-auto px-8">
-                <div id="" className="flex flex-row items-center gap-x-4">
+            <div id="nav-wrapper" className="absolute flex flex-row sm:justify-between justify-start items-center w-full h-auto px-8">
+                <div id="" className="flex flex-row items-center sm:gap-x-4">
                     <img src={blizzLogo} className="w-32 h-32"/>
                     <div id="links" className="lg:flex hidden flex-row 2xl:gap-x-4">
 
@@ -80,21 +82,100 @@ const NavbarComp = () => {
                         </Dropdown>
 
                     </div>
-                    <div id="" className="lg:hidden flex w-fit h-fit p-2 rounded-lg bg-zinc-500/10 hover:bg-zinc-500/40">
-                        <img src={dotsLogo} className="w-6 h-6" />
-                    </div>
+                    <Dropdown className="bg-[#1A1A1A]">
+                        <DropdownTrigger>
+                            <Button className="lg:hidden flex flex-row text-white text-lg font-bold bg-transparent hover:bg-zinc-500/40 border-none outline-none m-0 p-4">
+                                <img src={dotsLogo} className="w-6 h-6" />
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Games">
+                            {/* Drop-down sections with each being it's own series */}
+                            <DropdownSection key="Bnet Button">
+                                <DropdownItem key="button" href="">
+                                    <Button href="#" variant="flat" className="flex flex-row w-full h-auto py-2 bg-[#148CEE] text-white rounded-[2px] gap-x-4 justify-center items-center uppercase border-none outline-none">
+                                        <img src={bnetLogo} className="w-6 h-6" />
+                                        <span className="font-bold">Battle.NET</span>
+                                    </Button>
+                                </DropdownItem>
+                            </DropdownSection>
+                            <DropdownSection aria-label="Diablo Series" title="Diablo Series">
+                                <DropdownItem key="D4" href="https://diablo4.blizzard.com/en-us/">Diablo IV</DropdownItem>
+                                <DropdownItem key="D3" href="https://us.diablo3.blizzard.com/en-us/">Diablo III</DropdownItem>
+                                <DropdownItem key="D2R" href="https://diablo2.blizzard.com/en-us/">Diablo II: Resurrected</DropdownItem>
+                                <DropdownItem key="DI" href="https://diabloimmortal.blizzard.com/en-us/">Diablo Immortal</DropdownItem>
+                            </DropdownSection>
+                            <DropdownSection aria-label="Overwatch Series" showDivider={true} title="Overwatch Series">
+                                <DropdownItem key="OW" href="https://overwatch.blizzard.com/en-us/">Overwatch 2</DropdownItem>
+                            </DropdownSection>
+                            <DropdownSection aria-label="Warcraft Series" showDivider title="Warcraft Series">
+                                <DropdownItem key="WOW" href="https://worldofwarcraft.blizzard.com/en-us/">World of Warcraft</DropdownItem>
+                                <DropdownItem key="HS" href="https://hearthstone.blizzard.com/en-us">Hearthstone</DropdownItem>
+                                <DropdownItem key="WCRIII" href="https://warcraft3.blizzard.com/en-us/">Warcraft III: Reforged</DropdownItem>
+                                <DropdownItem key="WR" href="https://warcraftrumble.blizzard.com/en-us/">Warcraft Rumble</DropdownItem>
+                            </DropdownSection>
+                            <DropdownSection aria-label="Starcraft Series" title="Starcraft Series">
+                                <DropdownItem key="SC2" href="https://starcraft2.blizzard.com/en-us/">Starcraft</DropdownItem>
+                                <DropdownItem key="SCR" href="https://starcraft.blizzard.com/en-us/">Starcraft: Remastered</DropdownItem>
+                            </DropdownSection>
+                        </DropdownMenu>
+                    </Dropdown>
                 </div>
-                <div id="account-buttons" className="flex flex-row gap-x-8">
+                <div id="account-buttons" className="flex flex-row items-center lg:gap-x-8">
 
-                    <Link href="" className="text-white text-lg font-bold">About</Link>
+                    <Dropdown className="bg-[#1A1A1A]">
+                        <DropdownTrigger>
+                            <Button className="sm:flex hidden flex-row text-white text-lg font-bold bg-transparent hover:bg-zinc-500/40 border-none outline-none m-0 p-4">
+                                About
+                                <img src={chevronLogo} className="w-4 h-4" />
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu>
+                            {/* About dropdown stuff */}
+                            <DropdownSection aria-label="About Stuff">
+                            <   DropdownItem key="Account Blizzard">About Blizzard</DropdownItem>
+                                <DropdownItem key="News">News</DropdownItem>
+                                <DropdownItem key="BlizzCon">BlizzCon</DropdownItem>
+                                <DropdownItem key="Careers">Careers</DropdownItem>
+                            </DropdownSection>
+                        </DropdownMenu>
+                    </Dropdown>
 
-                    <Link href="https://us.shop.battle.net/en-us" className="text-white text-lg font-bold">Shop</Link>
-
-                    <Link href="" className="flex justify-center items-center p-[0.75rem] h-fit w-fit rounded-full bg-[#148CEE]">
-                        <img src={personLogo} className="w-6 h-6" />
+                    <Link href="https://us.shop.battle.net/en-us" className="sm:flex hidden text-white text-lg font-bold bg-transparent hover:bg-zinc-500/40 border-none outline-none m-0 p-2 rounded-lg">
+                        Shop
                     </Link>
 
-                    <Button href="#" variant="flat" className="xl:flex hidden flex-row w-[200px] h-[50px] bg-[#148CEE] text-white rounded-[2px] gap-x-4 justify-center items-center">
+                    <Dropdown className="bg-[#1A1A1A]">
+                        <DropdownTrigger>
+                            <Button className="flex justify-center sm:translate-x-[0px] translate-x-[20px] items-center p-[0.75rem] h-fit w-fit rounded-full bg-[#148CEE]">
+                                <img src={personLogo} className="w-6 h-6" />
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu>
+                            <DropdownSection key="Login Button">
+                                <DropdownItem key="login_button">
+                                    <Button href="#" variant="flat" className="flex flex-row w-full h-auto py-2 bg-[#148CEE] text-white rounded-[2px] gap-x-4 justify-center items-center border-none outline-none">
+                                        <img src={personLogo} className="w-4 h-4" />
+                                        <span className="font-bold">Login</span>
+                                    </Button>
+                                </DropdownItem>
+                            </DropdownSection>
+                            {/* Account setting sections and shop/about stuff when mobile*/}
+                            <DropdownSection aria-label="Account" showDivider title="Account">
+                                <DropdownItem key="Account Settings">Account Settings</DropdownItem>
+                                <DropdownItem key="Sign Up">Sign Up</DropdownItem>
+                                <DropdownItem key="Support">Support</DropdownItem>
+                                <DropdownItem key="Shop" className="sm:hidden flex">Shop</DropdownItem>
+                            </DropdownSection>
+                            <DropdownSection aria-label="About" showDivider title="About" className="sm:hidden">
+                                <DropdownItem key="Account Blizzard">About Blizzard</DropdownItem>
+                                <DropdownItem key="News">News</DropdownItem>
+                                <DropdownItem key="BlizzCon">BlizzCon</DropdownItem>
+                                <DropdownItem key="Careers" className="sm:hidden flex">Careers</DropdownItem>
+                            </DropdownSection>
+                        </DropdownMenu>
+                    </Dropdown>
+
+                    <Button href="#" variant="flat" className="xl:flex hidden flex-row w-[200px] h-[50px] bg-[#148CEE] text-white rounded-[5px] gap-x-4 justify-center items-center">
                         <img src={bnetLogo} className="translate-y-[3px] w-6 h-6" />
                         <span>Download B.NET</span>
                     </Button>
